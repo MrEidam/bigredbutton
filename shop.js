3821a3fd0a4fb963315965dd754a1d77d7171f7a
MrEidam <126872183+MrEidam@users.noreply.github.com>
2023-09-06 11:25:12 +0200

    Update shop.js

--- shop.js
+++ shop.js
@@ -33,7 +33,7 @@ function c(x){
 // Load
 function load(){
   items = JSON.parse(localStorage.getItem('items'));
-  cs = items.dogs + items.cats*12 + items.foxes*18 + items.wolfs*100 + items.hamsters*130 + items.whales*220 + items.capybaras*280 + items.platapuses*400 + items.porcupines*650 + items.hippos*950;
+  cs = items.dogs + items.cats*12 + items.foxes*18 + items.wolfs*100 + items.hamsters*130 + items.whales*220 + items.capybaras*280 + items.platapuses*400 + items.porcupines*650 + items.hippos*950 + items.snakes*1000 + items.cheetahs*3000;
 
   if(items.lang == 1){
     disclick.innerHTML=`You've got ${fornum(items.clicks)} clicks, and making ${fornum(cs)} c/s!`
